aa5cf31ce5cb5b2c32cba2e670a8ac8261dce289
Ryan Quey <quey.ryan@gmail.com>
2021-07-23 11:30:37 +0700

    add correlation map to abstract

--- components/DatasetDiscoveryVisualization/DiscoveryAbstract.vue
+++ components/DatasetDiscoveryVisualization/DiscoveryAbstract.vue
@@ -1,5 +1,12 @@
 <template>
   <div v-loading="isLoading || !osparcDataForChart" class="">
+  	<div class="chart-images">
+      <div class="chart-images-wrapper">
+        <div class="chart-image">
+          <img class="" :src="apiImageUrlBase()" />
+        </div>
+      </div>
+    </div>
     {{ osparcDataForChart }}
   </div>
 </template>
@@ -74,13 +81,49 @@ export default {
       const computed = base && base.outputs.output1.abstract
 
       return computed
-    }
+    },
+    pythonOsparcJobId () {
+      const base = this.$store.state.datasetComparison.osparcResults
+      return base && base.job_id
+    },
   },
 
   methods: {
+    apiImageUrlBase () { 
+      // for now, only one image, so no need for a param
+
+      const jobId = this.pythonOsparcJobId
+      if (!jobId) {
+        return false
+      }
+
+      const image_name = "Correlation_heatmap.png"
+      return `${process.env.flask_api_host}/api/results-images/${jobId}/${image_name}`
+
+
+    },
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.chart-images {
+  // TODO make mobile friendly
+  max-width: 100%;
+  .chart-images-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    .chart-image {
+      padding: 1rem;
+      box-sizing: border-box;
+      max-width: 50%;
+
+      img {
+        max-width: 100%;
+      }
+    }
+  }
+}
 </style>
